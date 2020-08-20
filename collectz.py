7883b9b01ab2d4aba93f888d5afd50ee99a5aff2
mfido <mikolaj.fido@gmail.com>
2020-08-20 22:16:27 +0100

    parse game params

--- collectz.py
+++ collectz.py
@@ -1,13 +1,37 @@
 import sys
+from math import min
+
+
+def get_params(line):
+    params = line.split()
+
+    if len(params) != 3:
+        raise ValueError
+    
+    return params
+
 
 def main():
     filename = sys.argv[1]
     
     with open(filename) as fp:
         line = fp.readline()
+        
+        # check first line format
+        # parse params
+        try:
+            x, y, z = get_params(line)
+        except ValueError:
+            return 'wrong first line'
+        
+        if z > min(x, y):
+            return 'wrong game'
+        
         while line:
             print(line)
             line = fp.readline()
 
+
 if __name__ == "__main__":
-    main()
+    result = main()
+    print(result)
